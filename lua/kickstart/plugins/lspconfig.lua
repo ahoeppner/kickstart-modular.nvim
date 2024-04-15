692615ed938d52fe4148a333550e2502dac0b6c5
Alex Hoeppner <alex@hwlocal.net>
2024-04-15 16:55:00 -0500

    working on lsp / linters

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -138,6 +138,13 @@ return {
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
+        pylint = {},
+        black = {},
+        pylsp = {},
+        mypy = {},
+        eslint_d = {},
+        prettier = {},
+        jsonls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
