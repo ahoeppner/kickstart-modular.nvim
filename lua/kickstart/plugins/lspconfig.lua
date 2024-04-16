906c81688d3d2fa2a55e7dbae724d4f2fc0a3e78
AlexanderHoeppner <alexander.hoeppner@target.com>
2024-04-16 14:33:34 -0500

    getting pylsp working

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -137,11 +137,36 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
-        pylint = {},
-        black = {},
-        pylsp = {},
-        mypy = {},
+        pylsp = {
+          settings = {
+            pylsp = {
+              plugins = {
+                ruff = {
+                  enabled = true, -- Enable the plugin
+                  formatEnabled = true, -- Enable formatting using ruffs formatter
+                  extendSelect = { 'I', 'W', 'E3', 'E5', 'C9', 'I', 'N', 'B', 'Q' }, -- Rules that are additionally used by ruff
+                  extendIgnore = {}, -- Rules that are additionally ignored by ruff
+                  format = { 'I' }, -- Rules that are marked as fixable by ruff that should be fixed when running textDocument/formatting
+                  --severities = { ['D212'] = 'I' }, -- Optional table of rules where a custom severity is desired
+                  unsafeFixes = false, -- Whether or not to offer unsafe fixes as code actions. Ignored with the "Fix All" action
+                  maxComplexity = 5,
+
+                  -- Rules that are ignored when a pyproject.toml or ruff.toml is present:
+                  lineLength = 88, -- Line length to pass to ruff checking and formatting
+                  showFixes = true,
+                  perFileIgnores = { ['__init__.py'] = 'CPY001' }, -- Rules that should be ignored for specific files
+                  preview = true, -- Whether to enable the preview style linting and formatting.
+                  targetVersion = 'py311', -- The minimum python version to target (applies for both linting and formatting).
+                },
+                mccabe = { enabled = false },
+                pycodestyle = { enabled = false },
+                pyflakes = { enabled = false },
+                autopep8 = { enabled = false },
+                yapf = { enabled = false },
+              },
+            },
+          },
+        },
         eslint_d = {},
         prettier = {},
         jsonls = {},
